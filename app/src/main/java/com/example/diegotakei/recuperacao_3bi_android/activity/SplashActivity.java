8df1262a5578715ad25886132dd6060d6c40223e
Diego Takei <diegotakei1586@gmail.com>
2016-02-06 17:43:37 -0300

    Correção na Splash

--- app/src/main/java/com/example/diegotakei/recuperacao_3bi_android/activity/SplashActivity.java
+++ app/src/main/java/com/example/diegotakei/recuperacao_3bi_android/activity/SplashActivity.java
@@ -1,19 +1,11 @@
 package com.example.diegotakei.recuperacao_3bi_android.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 
 import com.example.diegotakei.recuperacao_3bi_android.R;
 import com.example.diegotakei.recuperacao_3bi_android.asyncTask.StatusServer;
-import com.example.diegotakei.recuperacao_3bi_android.util.Response;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Diego Takei on 05/02/2016.
@@ -31,43 +23,8 @@ public class SplashActivity extends Activity implements Runnable {
     @Override
     public void run() {
 
-        String statusServer = null;
-        Response response = null;
-
-        StatusServer status = new StatusServer();
-        status.execute();
-
-        try {
-            response = status.get();
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-
-        try {
-
-            int status1;
-            status1 = response.getStatusCodeHttp();
-
-            if (status1 == 201) {
-
-                JSONObject json = new JSONObject(response.getContentValue());
-
-                statusServer = json.getString("online");
-            }
-
-        } catch (JSONException e) {
-
-            Log.e("Rec App", "JSONException: " + e);
-        }
-
-        if (statusServer.equals("true")) {
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        }
-        Log.e("valor", "server "+ statusServer);
-        finish();
+        StatusServer server = new StatusServer(SplashActivity.this);
+        server.execute();
 
     }
 }
--- app/src/main/java/com/example/diegotakei/recuperacao_3bi_android/asyncTask/StatusServer.java
+++ app/src/main/java/com/example/diegotakei/recuperacao_3bi_android/asyncTask/StatusServer.java
@@ -1,10 +1,15 @@
 package com.example.diegotakei.recuperacao_3bi_android.asyncTask;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.util.Log;
 
+import com.example.diegotakei.recuperacao_3bi_android.activity.MainActivity;
 import com.example.diegotakei.recuperacao_3bi_android.util.HttpService;
 import com.example.diegotakei.recuperacao_3bi_android.util.Response;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -13,6 +18,13 @@ import java.io.IOException;
  * Created by Diego Takei on 05/02/2016.
  */
 public class StatusServer extends AsyncTask<JSONObject, Void, Response> {
+
+    private Activity activity;
+
+    public StatusServer(Activity activity) {
+        this.activity = activity;
+    }
+
 
     @Override
     protected Response doInBackground(JSONObject... params) {
@@ -29,5 +41,33 @@ public class StatusServer extends AsyncTask<JSONObject, Void, Response> {
 
         return response;
     }
+    @Override
+     protected void onPostExecute(Response response) {
 
+        try {
+
+            int status1;
+            status1 = response.getStatusCodeHttp();
+
+            if (status1 == 201) {
+
+                JSONObject json = new JSONObject(response.getContentValue());
+
+                Boolean statusServer = json.getBoolean("online");
+
+                if(statusServer){
+                    Intent intent = new Intent(activity, MainActivity.class);
+                    activity.startActivity(intent);
+                    activity.finish();
+                }
+                else{
+                    activity.finish();
+                }
+            }
+
+        } catch (JSONException e) {
+
+            Log.e("Rec App", "JSONException: " + e);
+        }
+    }
 }
